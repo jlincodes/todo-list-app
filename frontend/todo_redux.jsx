c6ea91217a415055f81c84231f3a01b489dcf971
Julie <julie.lin0812@gmail.com>
2017-12-31 00:12:33 -0800

    Adds tests

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -6,6 +6,7 @@ import configureStore from './store/store';
 // for testing:
 // import { receiveAllTodos, receiveTodo } from './actions/todo_actions';
 // import App from './components/app';
+import {allTodos} from './reducers/selectors';
 
 import Root from './components/root';
 
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // window.receiveAllTodos = receiveAllTodos;
   // window.receiveTodo = receiveTodo;
   // ReactDOM.render(<App />, main);
+  // window.allTodos = allTodos;
 
   ReactDOM.render(<Root store={store}/>, main);
 });
