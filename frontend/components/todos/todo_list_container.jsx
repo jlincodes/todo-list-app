a1833ede3cef3e0ba5913d8857b1a54ad4596078
Julie <julie.lin0812@gmail.com>
2017-12-31 00:25:15 -0800

    Commits todo list container

--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -1,2 +1,18 @@
 import { connect } from 'react-redux';
 import TodoList from './todo_list';
+
+import { allTodos } from '../../reducers/selectors';
+import { receiveTodo, receiveAllTodos } from '../../actions/todo_actions';
+const mapStateToProps = (state) => ({
+  todos: allTodos(state)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  receiveTodo: (todo) => dispatch(receiveTodo(todo)),
+  receiveAllTodos: () => dispatch(receiveAllTodos())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
