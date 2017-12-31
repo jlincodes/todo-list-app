f29b2a7cd1d2e18abed4c45ac418318dc99c8ec0
Julie <julie.lin0812@gmail.com>
2017-12-31 00:02:02 -0800

    Refactors receive all todos action and adds sample state

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,16 +1,36 @@
 import {
   RECEIVE_ALL_TODOS,
-  RECEIVE_TODO
-} from '../actions/todo_actions';
+  RECEIVE_TODO } from '../actions/todo_actions';
 import merge from 'lodash/merge';
 
-let defaultState = {};
+// let defaultState = {};
+// for testing
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with dog shampoo',
+    done: true
+  }
+};
 
-const todosReducer = (state = defaultState, action) => {
-  Object.freezeState(state);
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let nextState;
   switch (action.type) {
     case RECEIVE_ALL_TODOS:
-      return action.todos;
+      // does not merge the old `todos` with the new `todos`
+      nextState = {};
+      action.todos.forEach(function(todo) {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
     case RECEIVE_TODO:
       return merge({}, state, {[action.todo.id]: action.todo});
     default:
