e7fdd4a772922b41533cf6c3c4465bb4407268c5
Julie <julie.lin0812@gmail.com>
2018-01-02 16:30:57 -0800

    Refactor todo list item component

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
+import TodoDetailViewContainer from './todo_detail_view_container';
 
 export default class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = { detail: false };
+    this.toggleDetail = this.toggleDetail.bind(this);
+    this.toggleTodo = this.toggleTodo.bind(this);
   }
 
+  toggleDetail(e) {
+    e.preventDefault();
+    this.setState({ detail: !this.state.detail });
+  }
+
+  toggleTodo(e) {
+    e.preventDefault();
+    const toggledTodo = Object.assign(
+      {},
+      this.props.todo,
+      {done: !this.props.todo.done }
+    );
+    this.props.receiveTodo(toggledTodo);
+  }
 
   render() {
     // deconstruct props object
-    const { todo } = this.props;
+    const { todo, removeTodo } = this.props;
     // deconstruct todo object
     const { title, done } = todo;
+
     let detail;
-
     if (this.state.detail) {
-      detail = todo.body;
+      detail = <TodoDetailViewContainer todo={ todo } />;
     }
-
-    console.log(todo.body);
-    console.log('detail', detail);
 
     return (
       <li className='todo-task'>
         <div>
           <h3>
-            {title}
+            <a onClick={ this.toggleDetail }>{ title }</a>
           </h3>
-          {detail}
+          <button className={ done ? 'Undo' : 'Done'} onClick={ this.toggleTodo }>
+            { done ? 'Undo' : 'Done'}
+          </button>
         </div>
+        { detail }
       </li>
     );
   }
